f74035fe8bd069abfe89b1b4495fea125fb432eb
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-03 09:09:45 +0300

    add: Added FNLNexusTests xtestplan for the FNLNexusTests target

---
+++ Tests/FNLNexusTests/CurcuitBreaker/FNLCircuitBreakerTests.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  FNLNexus
+//
+//  Created by Fenominall on 6/2/25.
+//
+
+import Foundation
+import FNLNexus
+import XCTest
+
+final class FNLCircuitBreakerTests: XCTestCase {
+    
+}
