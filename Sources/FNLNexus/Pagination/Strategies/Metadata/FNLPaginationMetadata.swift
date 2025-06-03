230eff831f2a408f3eda9079625429af9c3a822d
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-03 13:57:40 +0300

    add: Introduced "FNLPaginationMetadata' type as a helper protocol to define different types of Pagination aka keyset, offset, cursor based
    The type will be used by FNLAsyncPaginated to defined pagination strategies with the received metadata

--- Sources/FNLNexus/Pagination/Strategies/Metadata/FNLPaginationMetadata.swift
+++ Sources/FNLNexus/Pagination/Strategies/Metadata/FNLPaginationMetadata.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
+//  FNLPaginationMetadata.swift
 //  FNLNexus
 //
 //  Created by Fenominall on 6/3/25.
 //
 
 import Foundation
+
+public protocol FNLPaginationMetadata {
+    var hasMorePages: Bool { get }
+    
+    func nextQueryItems() -> [URLQueryItem]?
+}
