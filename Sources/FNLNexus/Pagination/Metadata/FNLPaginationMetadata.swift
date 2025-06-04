581c654aea48c72e587abd54d6415c18fade9650
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-04 21:33:44 +0300

    add: Added documentation to the 'FNLPaginationMetadata' protocol

--- Sources/FNLNexus/Pagination/Metadata/FNLPaginationMetadata.swift
+++ Sources/FNLNexus/Pagination/Metadata/FNLPaginationMetadata.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
+/// Describes pagination metadata used to determine if more pages exist,
+/// and how to build query items for fetching them.
 public protocol FNLPaginationMetadata {
+    /// Whether more pages are available.
     var hasMorePages: Bool { get }
-    
+
+    /// Generates URL query items for the next page.
     func nextQueryItems() -> [URLQueryItem]?
 }
+
