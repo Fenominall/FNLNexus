61ed2769fa234b27951de5414d922283f7d19dee
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-04 21:46:19 +0300

    feat: Add 'FNLPaginatedRequestBuilder' to construct a new endpoint for fetching the next page based on metadata working for pagination.

--- Sources/FNLNexus/Pagination/FNLPaginationRequestBuilder.swift
+++ Sources/FNLNexus/Pagination/FNLPaginationRequestBuilder.swift
@@ -1,8 +1,35 @@
 //
-//  File.swift
+//  FNLPaginationRequestBuilder.swift
 //  FNLNexus
 //
 //  Created by Fenominall on 6/3/25.
 //
 
 import Foundation
+
+/// Constructs a new endpoint for fetching the next page based on metadata.
+public struct FNLPaginatedRequestBuilder {
+    public init() {}
+    
+    public func buildNextPageEndpoint(
+        baseEndpoint: FNLEndpoint,
+        metadata: FNLPaginationMetadata
+    ) -> FNLEndpoint {
+        var nextItems = baseEndpoint.params ?? []
+        if let next = metadata.nextQueryItems() {
+            for item in next where !nextItems.contains(where: { $0.name == item.name }) {
+                nextItems.append(contentsOf: next)
+            }
+        }
+        
+        return FNLAPIEndpoint(
+            method: baseEndpoint.method,
+            headers: baseEndpoint.headers,
+            scheme: baseEndpoint.scheme,
+            host: baseEndpoint.host,
+            path: baseEndpoint.path,
+            body: baseEndpoint.body,
+            params: nextItems
+        )
+    }
+}
