8ebfb88420326803425d32c14187f2ffa677570a
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-04 21:43:19 +0300

    refactor: Fixed typo in the 'FNLPaginatedMapper'
    Added documentation

--- Sources/FNLNexus/Pagination/Mapper/FNLPaginatedMapper.swift
+++ Sources/FNLNexus/Pagination/Mapper/FNLPaginatedMapper.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 
-// Type that can be implemented for the reusable mapping and next page logic.
+/// A protocol for mapping raw input into paginated results and metadata.
 public protocol FNLPaginatedMapper: FNLMappable {
     associatedtype PageMetadata: FNLPaginationMetadata
-    
-    var medata: PageMetadata { get }
-    
+
+    /// Current page's metadata
+    var metadata: PageMetadata { get }
+
+    /// Maps a raw input into a page of outputs and new metadata.
     func mapPage(_ input: Input) async throws -> ([Output], PageMetadata)
 }
