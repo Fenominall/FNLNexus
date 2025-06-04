1fa86bbcba21522b5f84f008d097d10b0dbef26e
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-04 21:27:24 +0300

    chore: Moved pagination FNLPaginationMetadata files and MetaData strategies

--- Sources/FNLNexus/Pagination/Strategies/FNLPaginationStrategy.swift
+++ Sources/FNLNexus/Pagination/Strategies/FNLPaginationStrategy.swift
@@ -1,8 +1,17 @@
 //
-//  File.swift
+//  FNLPaginationStrategy.swift
 //  FNLNexus
 //
 //  Created by Fenominall on 6/3/25.
 //
 
 import Foundation
+
+/// A pagination strategy that can fetch a page of items.
+public protocol FNLPaginationStrategy {
+    associatedtype Item
+    associatedtype PageMetadata: FNLPaginationMetadata
+
+    /// Loads the current page and prepares for the next one.
+    func loadPage() async throws -> FNLAsyncPaginated<Item>
+}
