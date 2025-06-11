d4728ead38d7d4c47cdca09e19aa64f5ca33467d
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-11 16:58:40 +0300

    test[add]: Added MockEndpoint to the testing target

--- Tests/FNLNexusTests/Helpers/MockEndpoint.swift
+++ Tests/FNLNexusTests/Helpers/MockEndpoint.swift
@@ -9,29 +9,29 @@ import FNLNexus
 import Foundation
 
 struct MockEndpoint: FNLEndpoint {
-        var method: FNLRequestMethod
-        var headers: Header?
-        var scheme: FNLScheme
-        var host: String
-        var path: String
-        var body: FNLNexus.FNLBodyParameter?
-        var params: [URLQueryItem]?
-        
-        init(
-            method: FNLRequestMethod = .get,
-            headers: Header? = nil,
-            scheme: FNLScheme = .https,
-            host: String = "api.example.com",
-            path: String = "/test",
-            body: FNLBodyParameter? = nil,
-            params: [URLQueryItem]? = nil
-        ) {
-            self.method = method
-            self.headers = headers
-            self.scheme = scheme
-            self.host = host
-            self.path = path
-            self.body = body
-            self.params = params
-        }
+    var method: FNLRequestMethod
+    var headers: Header?
+    var scheme: FNLScheme
+    var host: String
+    var path: String
+    var body: FNLNexus.FNLBodyParameter?
+    var params: [URLQueryItem]?
+    
+    init(
+        method: FNLRequestMethod = .get,
+        headers: Header? = nil,
+        scheme: FNLScheme = .https,
+        host: String = "api.example.com",
+        path: String = "/test",
+        body: FNLBodyParameter? = nil,
+        params: [URLQueryItem]? = nil
+    ) {
+        self.method = method
+        self.headers = headers
+        self.scheme = scheme
+        self.host = host
+        self.path = path
+        self.body = body
+        self.params = params
     }
+}
