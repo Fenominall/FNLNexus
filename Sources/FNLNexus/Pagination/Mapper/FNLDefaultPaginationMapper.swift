07cc43acf3e06a777b1286ea1d4162b763522f0a
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-15 22:34:41 +0300

    add: Provided implementation of the 'FNLPaginationMapper' in the 'FNLDefaultPaginationMapper' by decoding FNLCodableRawResponse as Data and metadata as 'FNLPaginationMetadata'

--- Sources/FNLNexus/Pagination/Mapper/FNLDefaultPaginationMapper.swift
+++ Sources/FNLNexus/Pagination/Mapper/FNLDefaultPaginationMapper.swift
@@ -1,8 +1,21 @@
 //
-//  File.swift
+//  FNLDefaultPaginationMapper.swift
 //  FNLNexus
 //
 //  Created by Fenominall on 6/15/25.
 //
 
 import Foundation
+
+/// A default implementation of `FNLPaginatedMapper` for common JSON-based pagination.
+public struct FNLDefaultPaginationMapper<Item: Decodable, Metadata: Decodable & FNLPaginationMetadata>: FNLPaginatedMapper & Sendable {
+    public typealias PageMetadata = Metadata
+    public typealias Input = FNLCodableRawResponse
+    public typealias Output = [Item]
+
+    public func mapPage(_ input: FNLCodableRawResponse) async throws -> (Output, Metadata) {
+        let items = try JSONDecoder().decode([Item].self, from: input.data)
+        let metadata = try JSONDecoder().decode(Metadata.self, from: input.data)
+        return (items, metadata)
+    }
+}
