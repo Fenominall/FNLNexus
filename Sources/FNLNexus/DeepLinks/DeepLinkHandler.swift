0e236698f129607421579903f35fe745eb1d16ea
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-16 12:11:18 +0300

    feat[add]: - Added `DeepLinkHandler` protocol that handles the behavior for a given deep link. Can throw and supports async operations.

---
+++ Sources/FNLNexus/DeepLinks/DeepLinkHandler.swift
@@ -0,0 +1,13 @@
+//
+//  File 2.swift
+//  FNLNexus
+//
+//  Created by Fenominall on 6/16/25.
+//
+
+import Foundation
+
+/// Handles the behavior for a given deep link. Can throw and supports async operations.
+public protocol DeepLinkHandler: Sendable {
+    func handle() async throws
+}
