630d296112502043603077f7fb728367ba3e4a37
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-16 12:12:15 +0300

    feat[add]: Added 'DeeplinkHandlingResult' enum that indicates the result of a deep link handling attempt.

---
+++ Sources/FNLNexus/DeepLinks/DeepLinkHandlerResult.swift
@@ -0,0 +1,15 @@
+//
+//  File 2.swift
+//  FNLNexus
+//
+//  Created by Fenominall on 6/16/25.
+//
+
+import Foundation
+
+/// Indicates the result of a deep link handling attempt.
+public enum DeepLinkHandlingResult: Sendable {
+    case success
+    case failure(Error)
+    case cancelled
+}
