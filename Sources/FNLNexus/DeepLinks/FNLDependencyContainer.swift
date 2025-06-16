01d69e61400688ff865eaf7e72c81a886e8bec3e
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-16 12:50:25 +0300

    feat[add]: Added FNLDependencyContainer

--- Sources/FNLNexus/DeepLinks/FNLDependencyContainer.swift
+++ Sources/FNLNexus/DeepLinks/FNLDependencyContainer.swift
@@ -1,8 +1,12 @@
 //
-//  File 2.swift
+//  FNLDependencyContainer 2.swift
 //  FNLNexus
 //
 //  Created by Fenominall on 6/16/25.
 //
 
 import Foundation
+
+public protocol FNLDependencyContainer: Sendable {
+    func resolve<T>() throws -> T
+}
