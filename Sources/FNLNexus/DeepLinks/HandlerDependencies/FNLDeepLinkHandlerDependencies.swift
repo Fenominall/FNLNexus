3ec94bc107c51be54dda0bd6ac779dfadd115972
Vladislav Todorov <it.vtodorov@gmail.com>
2025-06-16 12:18:39 +0300

    feat[add]: Added FNLDeepLinkHandlerDependencies as a container for UI-related services passed to handlers

--- Sources/FNLNexus/DeepLinks/HandlerDependencies/FNLDeepLinkHandlerDependencies.swift
+++ Sources/FNLNexus/DeepLinks/HandlerDependencies/FNLDeepLinkHandlerDependencies.swift
@@ -1,8 +1,28 @@
 //
-//  File.swift
+//  FNLDeepLinkHandlerDependencies.swift
 //  FNLNexus
 //
 //  Created by Fenominall on 6/16/25.
 //
 
 import Foundation
+
+/// A container for UI-related services passed to handlers:
+public struct FNLDeepLinkHandlerDependencies: Sendable {
+    public let messageDisplayable: MessageDisplayable
+    public let activityDisplayable: ActivityDisplayable
+    public let contentReloadable: ContentReloadable
+    public let completionHandler: DeepLinkCompletionHandler
+    
+    public init(
+        messageDisplayable: MessageDisplayable,
+        activityDisplayable: ActivityDisplayable,
+        contentReloadable: ContentReloadable,
+        completionHandler: DeepLinkCompletionHandler
+    ) {
+        self.messageDisplayable = messageDisplayable
+        self.activityDisplayable = activityDisplayable
+        self.contentReloadable = contentReloadable
+        self.completionHandler = completionHandler
+    }
+}
